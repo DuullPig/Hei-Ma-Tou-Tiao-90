710379d7279bb8be453f07bccf68dff260b9e06b
LJJ <LiJunJie200091@163.com>
2019-12-20 20:24:54 +0800

    黑马头条PC-主页模块-头部结构及下拉菜单

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -8,7 +8,9 @@
     <!-- 右侧 -->
     <el-container>
       <!-- 上下布局 -->
-      <el-header>头部</el-header>
+      <el-header>
+        <layout-header></layout-header>
+      </el-header>
       <el-main>主要区域</el-main>
     </el-container>
   </el-container>
@@ -16,9 +18,11 @@
 
 <script>
 import layoutAside from '../../components/home/layout-aside'
+import layoutHeader from '../../components/home/layout-header'
 export default {
   components: {
-    'layout-aside': layoutAside
+    'layout-aside': layoutAside,
+    'layout-header': layoutHeader
   }
 }
 </script>
