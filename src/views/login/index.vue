7f8e4d5cd02e2195cb91bc92b8b51be0a14d87aa
LJJ <LiJunJie200091@163.com>
2019-12-20 15:06:55 +0800

    黑马头条PC-登录模块-准备工作

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,20 +5,20 @@
       <div class="title">
         <img src="../../assets/img/logo_index.png" alt="">
       </div>
-      <!-- 放置表单 -->
-      <el-form>
+      <!-- 放置表单  绑定一个属性-->
+      <el-form ：model="loginForm" :rules="loginRules">
         <!-- 表单域 里面的设置 -->
-        <el-form-item>
-          <el-input placeholder="请输入手机号"></el-input>
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
         <!-- 表单域 -->
-        <el-form-item>
-          <el-input style="width:65%" placeholder="请输入验证码"></el-input>
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" style="width:65%" placeholder="请输入验证码"></el-input>
           <el-button style="float:right" plain>发送验证码</el-button>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="check">
           <!-- 复选框 -->
-           <el-checkbox>我以阅读并同意用户协议和隐私条款</el-checkbox>
+           <el-checkbox v-model="loginForm.check">我以阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
          <el-button type="primary" style="width:100%">登录</el-button>
@@ -30,7 +30,18 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        check: false // 是否勾选
+      },
+      loginRules: {
+        // 验证对象
+      }
+    }
+  }
 }
 </script>
 
