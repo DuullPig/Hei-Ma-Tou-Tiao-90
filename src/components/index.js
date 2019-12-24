b1207aeaa7933dc1c581540d5373141f35d17a8b
LJJ <LiJunJie200091@163.com>
2019-12-24 22:41:40 +0800

    黑马头条PC-面包屑组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 import layoutAside from './home/layout-aside.vue'
 import layoutHeader from './home/layout-header.vue'
+import breadCrumb from './common/bread-crumb.vue'
 export default {
   install (Vue) {
     Vue.component('layout-aside', layoutAside) // 注册一个全局组件
     Vue.component('layout-header', layoutHeader) // 注册一个全局组件
+    Vue.component('bread-crumb', breadCrumb) // 全局注册一个面包屑组件
   }
 }
