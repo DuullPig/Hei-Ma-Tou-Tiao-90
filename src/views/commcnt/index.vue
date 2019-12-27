89538c200f51ec62dc927ae038961b7d5b8940e2
LJJ <LiJunJie200091@163.com>
2019-12-27 10:31:01 +0800

    黑马头条PC-评论列表-评论数据加载到页面及状态过滤

--- src/views/commcnt/index.vue
+++ src/views/commcnt/index.vue
@@ -8,20 +8,49 @@
         </template>
     </bread-crumb>
     <!-- 组件表格 -->
-    <el-table>
-        <!-- 表格 -->
-        <el-table-column width="450px" label="标题"></el-table-column>
-        <el-table-column label="评论状态"></el-table-column>
-        <el-table-column label="总评论数"></el-table-column>
-        <el-table-column label="粉丝评论数"></el-table-column>
-        <el-table-column label="操作"></el-table-column>
+    <el-table :data="list">
+        <!-- 表格  width宽度 label表头 prop字段名-->
+        <el-table-column prop="title" width="550px" label="标题"></el-table-column>
+        <el-table-column   :formatter="formatterBoolean"  prop="comment_status" label="评论状态"></el-table-column>
+        <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
+        <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+        <el-table-column prop="" label="操作">
+          <template slot-scope="obj">
+             <el-button size="small" type="text">修改</el-button>
+          <el-button size="small" type="text">{{obj.row.comment_status ? '关闭评论' : '打开评论'}}</el-button>
+          </template>
+
+        </el-table-column>
     </el-table>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: [] // 定义一个数据返回结果
+    }
+  },
+  methods: {
+    getComment () {
+      this.$axios({
+        url: '/articles',
+        params: { response_type: 'comment' }
+      }).then(result => {
+        this.list = result.data.results
+      })
+    },
+    // 定义一个格式化函数
+    // row当前行数据, column当前列表数据, cellValue当前单元格值, index下标
+    formatterBoolean (row, column, cellValue, index) {
+      // return cellValue ? '正常' : '关闭1'
+      return cellValue ? '正常' : '关闭'
+    }
+  },
+  created () {
+    this.getComment() // 获取数据
+  }
 }
 </script>
 
