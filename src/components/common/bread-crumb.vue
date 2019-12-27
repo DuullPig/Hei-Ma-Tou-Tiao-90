3c0bb4872ddb63f5c4a05e1b5bdca5a499224237
LJJ <LiJunJie200091@163.com>
2019-12-27 09:03:04 +0800

    黑马头条PC-评论列表-页面结构-table

--- src/components/common/bread-crumb.vue
+++ src/components/common/bread-crumb.vue
@@ -1,7 +1,8 @@
 <template>
-  <el-breadcrumb>
+  <el-breadcrumb separator=">">
       <el-breadcrumb-item to="/home">首页</el-breadcrumb-item>
        <el-breadcrumb-item>
+         <!-- 定义一个具名插槽 -->
            <slot name="title"></slot>
        </el-breadcrumb-item>
   </el-breadcrumb>
--- src/views/commcnt/index.vue
+++ src/views/commcnt/index.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>评论列表</div>
+  <!-- 卡片组件 -->
+  <el-card>
+    <bread-crumb slot="header">
+        <!-- 插槽内容 -->
+        <template slot="title">
+            评论管理
+        </template>
+    </bread-crumb>
+    <!-- 组件表格 -->
+    <el-table>
+        <!-- 表格 -->
+        <el-table-column width="450px" label="标题"></el-table-column>
+        <el-table-column label="评论状态"></el-table-column>
+        <el-table-column label="总评论数"></el-table-column>
+        <el-table-column label="粉丝评论数"></el-table-column>
+        <el-table-column label="操作"></el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
