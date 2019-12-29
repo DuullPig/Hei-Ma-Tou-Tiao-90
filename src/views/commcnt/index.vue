0cb4c68f904ddcef9c41d9f0b283581c25573ce1
LJJ <LiJunJie200091@163.com>
2019-12-29 22:50:53 +0800

    黑马头条PC-评论列表-列表分页及请求

--- src/views/commcnt/index.vue
+++ src/views/commcnt/index.vue
@@ -2,26 +2,32 @@
   <!-- 卡片组件 -->
   <el-card>
     <bread-crumb slot="header">
-        <!-- 插槽内容 -->
-        <template slot="title">
-            评论管理
-        </template>
+      <!-- 插槽内容 -->
+      <template slot="title">评论管理</template>
     </bread-crumb>
     <!-- 组件表格 -->
     <el-table :data="list">
-        <!-- 表格  width宽度 label表头 prop字段名-->
-        <el-table-column prop="title" width="550px" label="标题"></el-table-column>
-        <el-table-column   :formatter="formatterBoolean"  prop="comment_status" label="评论状态"></el-table-column>
-        <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
-        <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
-        <el-table-column prop="" label="操作">
-          <template slot-scope="obj">
-             <el-button size="small" type="text">修改</el-button>
-          <el-button @click="openOrCloseState(obj.row)" size="small" type="text">{{obj.row.comment_status ? '关闭评论' : '打开评论'}}</el-button>
-          </template>
-
-        </el-table-column>
+      <!-- 表格  width宽度 label表头 prop字段名-->
+      <el-table-column prop="title" width="550px" label="标题"></el-table-column>
+      <el-table-column :formatter="formatterBoolean" prop="comment_status" label="评论状态"></el-table-column>
+      <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
+      <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+      <el-table-column prop label="操作">
+        <template slot-scope="obj">
+          <el-button size="small" type="text">修改</el-button>
+          <el-button
+            @click="openOrCloseState(obj.row)"
+            size="small"
+            type="text"
+          >{{obj.row.comment_status ? '关闭评论' : '打开评论'}}</el-button>
+        </template>
+      </el-table-column>
     </el-table>
+    <el-row type='flex' justify="center" align="middle" style="height:80px">
+      <!-- 分页组件 -->
+      <el-pagination background layout="prev, pager, next" :current-page='page.currentPage' :page-size="page.pageSize"
+      :total="page.total" @current-change="changePage"></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -29,16 +35,27 @@
 export default {
   data () {
     return {
-      list: [] // 定义一个数据返回结果
+      list: [], // 定义一个数据返回结果
+      page: {
+        total: 0,
+        pageSize: 10, // 默认每页条数
+        currentPage: 1 // 默认每页页码
+      }
     }
   },
   methods: {
+    // 页面改变事件
+    changePage (newPage) {
+      this.page.currentPage = newPage // 最新页面
+      this.getComment()
+    },
     getComment () {
       this.$axios({
         url: '/articles',
-        params: { response_type: 'comment' }
+        params: { response_type: 'comment', page: this.page.currentPage, per_page: this.page.pageSize }
       }).then(result => {
         this.list = result.data.results
+        this.page.total = result.data.total_count
       })
     },
     // 定义一个格式化函数
@@ -58,12 +75,14 @@ export default {
           url: '/comments/status',
           params: { article_id: row.id.toString() },
           data: { allow_comment: !row.comment_status }
-        }).then(result => {
-          debugger
-          this.getComment()
-        }).catch(() => {
-          debugger
         })
+          .then(result => {
+            debugger
+            this.getComment()
+          })
+          .catch(() => {
+            debugger
+          })
       })
     }
   },
@@ -74,5 +93,4 @@ export default {
 </script>
 
 <style>
-
 </style>
